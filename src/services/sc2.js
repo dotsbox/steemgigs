f3075177b8d18e91463a58ec93f75a5fa5402108
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2018-08-01 23:18:55 +0800

    Update sc2.js

--- src/services/sc2.js
+++ src/services/sc2.js
@@ -2,9 +2,9 @@ import sc2 from 'sc2-sdk'
 
 const sc = sc2.Initialize({
   baseURL: 'https://v2.steemconnect.com',
-  app: 'steemgigs.app',
-  // app: 'steemgig.app',
-  callbackURL: 'https://steemgigs.org/complete',
+  app: 'casteem',
+  // app: 'casteem',
+  callbackURL: 'https://steem.racing/complete',
   // callbackURL: 'http://localhost:8080/complete',
   // scope: ['login', 'vote', 'comment', 'comment_delete', 'comment_options', 'custom_json', 'claim_reward_balance'] // wrong
   scope: ['vote', 'comment', 'delete_comment', 'comment_options', 'custom_json', 'claim_reward_balance', 'login', 'offline'] //right (fixed!)
